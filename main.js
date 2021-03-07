eb88be737df9fcc102ce2bc227dfb8ee14f98136
Diriector_Doc <davijust213@yahoo.ca>
2021-03-07 02:08:39 -0500

    New method for getting elements

--- main.js
+++ main.js
@@ -6,18 +6,19 @@ $(function(){
 	let today = new Date,
 		time = `${today.getHours() ? today.getHours() > 12 ? today.getHours() - 12 : today.getHours() : 12}:${today.getMinutes() < 10 ? "0"+today.getMinutes() : today.getMinutes()} ${today.getHours() > 11 ? "PM" : "AM"}`;
 	$(".embed-footer-text").html("Today at "+time)
+	$('[field="birthday"]').html(`August 19, 2020\n(${(Date.now()-15978096e5)/315576e5|0} year${(Date.now()-15978096e5)/315576e5|0?"":"s"} ago)`)
 		
-	let about = $("h2[about],h2[about]+p,h2[about]+p+h3,h2[about]+p+h3+div,h2[about]+p+h3+div+h3,h2[about]+p+h3+div+h3+p"),
-		bug = $("h2[bug],h2[bug]+p,h2[bug]+p+div,h2[bug]+p+div+h3,h2[bug]+p+div+h3+p"),
-		commands = $("h2[commands],h2[commands]+p,h2[commands]+p+h3,h2[commands]+p+h3+div,h2[commands]+p+h3+div+h3,h2[commands]+p+h3+div+h3+p"),
-		colour = $("h2[colour],h2[colour]+div,h2[colour]+div+p,h2[colour]+div+p+h3,h2[colour]+div+p+h3+p,h2[colour]+div+p+h3+p+ul,h2[colour]+div+p+h3+p+ul+p,h2[colour]+div+p+h3+p+ul+p+h3,h2[colour]+div+p+h3+p+ul+p+h3+p,h2[colour]+div+p+h3+p+ul+p+h3+p+pre,h2[colour]+div+p+h3+p+ul+p+h3+p+pre+p,h2[colour]+div+p+h3+p+ul+p+h3+p+pre+p+pre,h2[colour]+div+p+h3+p+ul+p+h3+p+pre+p+pre+p"),
-		notify = $("h2[notify],h2[notify]+p,h2[notify]+p+p"),
-		request = $("h2[request],h2[request]+p,h2[request]+p+h3,h2[request]+p+h3+p,h2[request]+p+h3+p+pre,h2[request]+p+h3+p+pre+h4,h2[request]+p+h3+p+pre+h4+p,h2[request]+p+h3+p+pre+h4+p+p,h2[request]+p+h3+p+pre+h4+p+p+ul,h2[request]+p+h3+p+pre+h4+p+p+ul+h4,h2[request]+p+h3+p+pre+h4+p+p+ul+h4+p,h2[request]+p+h3+p+pre+h4+p+p+ul+h4+p+p,h2[request]+p+h3+p+pre+h4+p+p+ul+h4+p+p+h3,h2[request]+p+h3+p+pre+h4+p+p+ul+h4+p+p+h3+pre"),
-		speedrun = $("h2[speedrun],h2[speedrun]+p,h2[speedrun]+p+h3,h2[speedrun]+p+h3+p,h2[speedrun]+p+h3+p+pre,h2[speedrun]+p+h3+p+pre+h4,h2[speedrun]+p+h3+p+pre+h4+p,h2[speedrun]+p+h3+p+pre+h4+p+ul,h2[speedrun]+p+h3+p+pre+h4+p+ul+h4,h2[speedrun]+p+h3+p+pre+h4+p+ul+h4+p,h2[speedrun]+p+h3+p+pre+h4+p+ul+h4+p+ul,h2[speedrun]+p+h3+p+pre+h4+p+ul+h4+p+ul+p,h2[speedrun]+p+h3+p+pre+h4+p+ul+h4+p+ul+p+ul,h2[speedrun]+p+h3+p+pre+h4+p+ul+h4+p+ul+p+ul+p,h2[speedrun]+p+h3+p+pre+h4+p+ul+h4+p+ul+p+ul+p+ul,h2[speedrun]+p+h3+p+pre+h4+p+ul+h4+p+ul+p+ul+p+ul+h3,h2[speedrun]+p+h3+p+pre+h4+p+ul+h4+p+ul+p+ul+p+ul+h3+pre,h2[speedrun]+p+h3+p+pre+h4+p+ul+h4+p+ul+p+ul+p+ul+h3+pre+div"),
-		wisdom = $("h2[wisdom],h2[wisdom]+p,h2[wisdom]+p+h3,h2[wisdom]+p+h3+p"),
-		yellatme = $("h2[yellatme],h2[yellatme]+p,h2[yellatme]+p+div,h2[yellatme]+p+div+h3,h2[yellatme]+p+div+h3+p"),
-		help = $("h2[help],h2[help]+p,h2[help]+p+ul,h2[help]+p+ul+h3,h2[help]+p+ul+h3+pre,h2[help]+p+ul+h3+pre+p"),
-		secret = $("h2[secret],h2[secret]+p");
+	let about = $("#info>*").slice(0, 6),
+		bug = $("#info>*").slice(6, 11),
+		commands = $("#info>*").slice(11, 17),
+		colour = $("#info>*").slice(17, 30),
+		notify = $("#info>*").slice(30, 33),
+		request = $("#info>*").slice(33, 47),
+		speedrun = $("#info>*").slice(47, 65),
+		wisdom = $("#info>*").slice(65, 69),
+		yellatme = $("#info>*").slice(69, 74),
+		help = $("#info>*").slice(74, 80),
+		secret = $("#info>*").slice(80);
 	
 	function loadInfo(hash){
 		$(".active").removeClass("active")
@@ -62,11 +63,12 @@ $(function(){
 				$("#info").html(secret);
 				$('tr[house="secret"]').addClass("active")
 				break;
-			default:
-				window.location = "#";
 			case "#about":
 				$("#info").html(about);
 				$('tr[house="about"]').addClass("active")
+				break;
+			default:
+				window.location = "#";
 		}
 	}
 	
